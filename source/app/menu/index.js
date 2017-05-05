b63287448ab7227581b7e0beae0e9fbb8df65455
RFateev <RFateev@MacBook-Pro-Ruslan.local>
2017-05-05 12:49:37 +0300

    Portfolio-9. Menu sliding refactored

--- source/app/menu/index.js
+++ source/app/menu/index.js
@@ -4,6 +4,9 @@ $(document).ready(() => {
   const openBtn = $('.header-top__menu-btn')
   const closeBtn = $('.main-menu__svg-container')
   const menu = $('.main-menu')
+  const leftSide = $('.main-menu__left')
+  const rightSide = $('.main-menu__right')
+  const menuLinks = $('.main-menu__link')
   const body = $('body')
 
   const MENU_ACTIONS = {
@@ -11,16 +14,46 @@ $(document).ready(() => {
     close: 'close',
   }
 
+  const animation = (selector, props, duration, cb) => selector.animate({ ...props }, duration, cb)
+
+  const bindAction = action => {
+    if (action === 'unbind') {
+      openBtn.unbind()
+      closeBtn.unbind()
+    } else {
+      openBtn.bind('click', () => manageMenu(MENU_ACTIONS.open))
+      closeBtn.bind('click', () => manageMenu(MENU_ACTIONS.close))
+    }
+  }
+
+  const showMenuItems = (index = 0) => {
+    const lastLink = menuLinks.length - 1
+    if (index <= lastLink) {
+      $(menuLinks[index]).fadeIn(200, () => showMenuItems(index + 1))
+    } else {
+      bindAction()
+    }
+  }
+
   const manageMenu = action => {
+    bindAction('unbind')
     if (action === MENU_ACTIONS.open) {
-      menu.slideDown(500).css('display', 'flex')
+      menu.css('display', 'flex')
       body.css('overflow', 'hidden')
+      animation(leftSide, {'left': 0}, 500)
+      animation(rightSide, {'left': '50%'}, 500, showMenuItems)
     } else if (action === MENU_ACTIONS.close) {
-      menu.slideUp(500)
+      const menuCloseCb = () => {
+        leftSide.css('left', '-50%')
+        rightSide.css('left', '100%')
+        menuLinks.fadeOut()
+        bindAction()
+      }
+
+      menu.fadeOut(200, menuCloseCb)
       body.css('overflow', 'visible')
     }
   }
 
-  openBtn.on('click', () => manageMenu(MENU_ACTIONS.open))
-  closeBtn.on('click', () => manageMenu(MENU_ACTIONS.close))
+  bindAction()
 })
